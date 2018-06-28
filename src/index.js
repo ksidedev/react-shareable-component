a3a020edc08cfa38e8164aeeb8bac64912d58465
Deji Beckley <dejibeckley@STLs-MacBook-Pro-3.local>
2018-06-28 00:27:15 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Fancy extends Component {
+  render() {
+    return (
+      <div>
+      	<p>This is so Fancy!</p>
+      </div>
+    );
+  }
+}
+export default Fancy;
