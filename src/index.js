21614c279847f3c78cb38e1c9ce7de809fc1e72f
Deji Beckley <dejibeckley@STLs-MacBook-Pro-3.local>
2018-06-28 02:15:46 +0000

    adding a component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import ComponentOne from './components/ComponentOne'
 class ComponentWrapper extends Component {
   render() {
     return (
       <div>
         <p>This is so Fancy!</p>
+        <ComponentOne />
       </div>
     )
   }
